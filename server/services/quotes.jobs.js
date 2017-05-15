2726a39817a6e9ea8226119a4c5213886edd14f6
carloscalderon <cdcalderon@gmail.com>
2017-05-14 19:45:35 -0500

    add momentjs

--- server/services/quotes.jobs.js
+++ server/services/quotes.jobs.js
@@ -1,5 +1,6 @@
 const indicatorsUtils = require('../common/indicatorsUtils');
 const { StockQuote } = require('./../models/stockQuote');
+const moment = require('moment');
 
 let getHistoricalQuotes = (symbol, from, to, resolve, reject) => {
     return Promise.resolve(indicatorsUtils.getHistoricalQuotes(symbol,from,to,resolve, reject));
