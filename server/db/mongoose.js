6c28847296b8a25811c4d691cc3a37a8c7d0c92d
carloscalderon <cdcalderon@gmail.com>
2017-07-30 16:05:32 -0500

    refactor MongoDB connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 //import mongoose from 'mongoose';
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
+let dbURI = 'mongodb://localhost:27017/StockSignal';
+//let dbURI = 'mongodb://stocksignal>:6tN14aiavnTK6aLfaPEETqlf5R91PGYOYkuPsNWFr40L7sMNWg3ukGpx1k5pGtYWUNWw9UwSFohoKjchLyZoZw==@stocksignal.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 
-                 'mongodb://localhost:27017/StockSignal');
+mongoose.connect(process.env.MONGODB_URI || dbURI);
 
 module.exports = { mongoose };
