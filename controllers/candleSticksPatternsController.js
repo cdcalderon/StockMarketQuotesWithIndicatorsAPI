3740ecad713ff6887ae6f7ce3dd96dbde6aa2ef1
carloscalderon <cdcalderon@gmail.com>
2017-11-15 23:40:56 -0600

    Make dummy change to (POC) by getting invalid volume

--- controllers/candleSticksPatternsController.js
+++ controllers/candleSticksPatternsController.js
@@ -1,19 +1,14 @@
-const filterComposer = require('../server/common/filterComposerUtils');
-const stockMarketQuotesService = require('../server/common/stockMarketQuotesService');
 
-let candleSticksPatternsController = (GapSignal, quotes) => {
+let candleSticksPatternsController = () => {
     let get = (req, res) => {
-        let symbols = req.body.params.symbols;
-        let from = new Date(req.body.params.from);
-        let to = new Date(req.body.params.to);
-
         res.send({
             date: '05/07/2017',
             type: "doji",
-            volume: 23444444
+            volume: 444444455666
         });
 
     };
+
 
 
     return {
